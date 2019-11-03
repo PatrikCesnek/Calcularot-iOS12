7a4718f724c85b83c63917c32a6709af0d9871d4
PatrikCesnek <46940455+PatrikCesnek@users.noreply.github.com>
2019-11-03 11:39:17 +0100

    App done for iOS 12

---
+++ Auto Layout Calculator/Auto Layout Calculator/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  Auto Layout Calculator
+//
+//  Created by Patrik Cesnek on 23/05/2019.
+//  Copyright © 2019 Patrik Cesnek. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var result: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        result.text = "\(0)"
+    }
+
+    
+
+}
+
